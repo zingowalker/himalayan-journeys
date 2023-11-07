b58207ad675da4929d097a1ec491c9b7c34f8b27
Tsetan Dorje <tsetangrace@gmail.com>
2023-11-08 00:10:56 +0530

    reset

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,7 +5,7 @@ import logoImg from "@/public/Dae6.png";
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
-      <div className="w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4 sm:items-center justify-center">
+      <div className="w-11 md:w-14 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4 sm:items-center justify-center">
         <Image
           src={logoImg}
           alt="eco himalayan journeys"
@@ -14,7 +14,7 @@ const Logo = () => {
           priority
         />
       </div>
-      <span className="font-normal dark:font-semibold text-lg md:text-xl">
+      <span className="font-normal dark:font-semibold text-lg md:text-xl hidden">
         Eco Himalayan Journeys
       </span>
     </Link>
