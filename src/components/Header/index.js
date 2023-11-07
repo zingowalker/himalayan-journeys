942204f52563908c4af342fe0ab7d901b7b491ed
Tsetan Dorje <tsetangrace@gmail.com>
2023-11-07 12:57:34 +0530

    nav style changed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -64,7 +64,7 @@ const Header = () => {
       </button>
 
       <nav
-        className="w-max py-3 px-6 sm:px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center flex  sm:hidden
+        className="w-max py-3 px-6 sm:px-8 border border-solid border-dark rounded-md font-medium capitalize  items-center flex  sm:hidden
         fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50
         transition-all ease duration-300
         "
@@ -102,7 +102,7 @@ const Header = () => {
       </nav>
 
       <nav
-        className=" w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center hidden sm:flex
+        className="w-max py-3 px-8 border border-solid border-dark rounded-md font-medium capitalize items-center hidden sm:flex
         fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50"
       >
         <Link href="/" className="mr-2">
