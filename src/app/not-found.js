39f288c399aa1c0755e9c08acc41cdea7192e152
Tsetan Dorje <tsetangrace@gmail.com>
2023-12-10 19:32:40 +0530

    404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,41 +1,35 @@
 import Link from "next/link";
-import { FaArrowLeft } from "react-icons/fa";
 
 export const metadata = {
   title: "Error: 404",
 };
 
-const NotFound = () => {
+export default function NotFound() {
   return (
-    <section>
-      <div className="py-10 px-4 dark:text-light">
-        <div className="text-center">
-          <p className="text-base font-semibold ">Error code: 404</p>
-          <h1 className="mt-2 text-lg font-semibold tracking-tight  sm:text-5xl">
-            Oops! We can't seem to find the page you're looking for.
-          </h1>
-          <p className="mt-4 text-base leading-7 text-gray-600">
-            You may have mistyped the address or the page may have moved.
-          </p>
-          <div className="mt-4 flex items-center justify-center gap-x-3">
-            <Link
-              href={"/"}
-              className="inline-flex items-center rounded-md border border-black px-3 py-2 text-sm font-semibold  shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-            >
-              <FaArrowLeft size={16} className="mr-2" />
-              Go back
-            </Link>
-            <Link
-              href={"/contact"}
-              className="rounded-md  px-3 py-2 text-sm font-semibold shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-            >
-              Contact us
-            </Link>
-          </div>
-        </div>
+    <main className="py-32 w-full dark:bg-dark flex justify-center font-mr">
+      <div className="relative flex flex-col items-center justify-center">
+        <h1
+          className={`inline-block text-dark dark:text-light
+      text-base font-bold w-full xl:text-4xl text-center`}
+        >
+          Oops! We can't seem to find the page you're looking for.
+        </h1>
+        <h2
+          className={`inline-block text-dark dark:text-light
+      text-base font-semibold w-full xl:text-2xl text-center mt-4 tracking-wide leading-snug`}
+        >
+          You may have mistyped the address or the page may have moved.
+        </h2>
+        <Link
+          href="/"
+          className="self-center mt-8 inline-block rounded-lg border-2 border-solid bg-dark px-4 py-2
+        font-semibold text-light hover:border-dark hover:bg-light hover:text-dark 
+        dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
+        "
+        >
+          Go To Home
+        </Link>
       </div>
-    </section>
+    </main>
   );
-};
-
-export default NotFound;
+}
