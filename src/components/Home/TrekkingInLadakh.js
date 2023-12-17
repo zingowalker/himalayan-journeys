0169310e55020478848a89e54a62e27cb0bc5e3c
Tsetan Dorje <tsetangrace@gmail.com>
2023-12-17 18:40:33 +0530

    layout style

--- src/components/Home/TrekkingInLadakh.js
+++ src/components/Home/TrekkingInLadakh.js
@@ -15,48 +15,17 @@ import { trekData } from "@/data/trekData";
 const TrekkingInLadakh = () => {
   // const sortedItineraries = sortItineraries(itineraries);
   return (
-    <section className="w-full  p-6 mt-16 sm:mt-24 md:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center dark:text-light">
+    <section className="w-full mt-16 sm:mt-24  md:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center text-dark dark:text-light">
       <div className="w-full inline-block">
-        <div className="mb-2">
-          <h2 className="font-bold text-2xl md:text-4xl text-dark dark:text-light">
-            Trekking in Ladakh
-          </h2>
-        </div>
-        <p className="font-normal text-lg md:text-2xl text-dark dark:text-light">
+        <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl ">
+          Trekking in Ladakh
+        </h2>
+        <p className="font-normal text-lg md:text-2xl">
           Most popular trekking in Ladakh for travellers from around the world.
         </p>
       </div>
 
-      <div className="flex space-x-4 py-5 overflow-x-scroll">
-        {/* {sortedItineraries.slice(12, 16).map((itinerary, index) => (
-          <article key={index} className="space-y-1 shrink-0 cursor-pointer">
-            <Link href={itinerary.url} className="overflow-hidden">
-              <Image
-                src={itinerary.image.filePath.replace("../public", "")}
-                placeholder="blur"
-                blurDataURL={itinerary.image.blurhashDataUrl}
-                alt={itinerary.title}
-                width="120"
-                height="100"
-                className="w-80 h-72 object-cover rounded-lg pb-2"
-              />
-            </Link>
-
-            <div className="flex flex-col">
-              <Tag2
-                link={`/categories/${slug(itinerary.tags[0])}`}
-                name={itinerary.tags[0]}
-              />
-              <div className="flex gap-4 mt-3">
-                <GiCampingTent />
-                <TbTrekking />
-                <MdNoMeals />
-                <FaHorse />
-                <LuChefHat />
-              </div>
-            </div>
-          </article>
-        ))} */}
+      <article className="flex space-x-4 py-5 overflow-x-scroll mt-10 sm:mt-16 max-w-7xl mx-auto p-6">
         {trekData.map((item) => (
           <div key={item.id} className="space-y-1 shrink-0 cursor-pointer">
             <Link href={item.url}>
@@ -66,14 +35,13 @@ const TrekkingInLadakh = () => {
                 src={item.src}
                 alt=""
               />
-
               <p className="font-bold">{item.title}</p>
             </Link>
             <p className="">Grade: {item.grade}</p>
             <p className="font-light text-sm">Best time: {item.bestTime}</p>
           </div>
         ))}
-      </div>
+      </article>
     </section>
   );
 };
