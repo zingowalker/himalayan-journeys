ba1f25565fdd54ed403b94268f265bc508207296
Tsetan Dorje <tsetangrace@gmail.com>
2023-12-18 15:13:00 +0530

    home cover image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -45,7 +45,7 @@ export default function Header() {
   return (
     <Disclosure
       as="nav"
-      className={`text-light w-full fixed top-0 left-0 right-0 z-50  transition-all duration-300 overflow-x-hidden ${
+      className={`text-light w-full fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
         scrolled ? "bg-[#013B94]" : "bg-transparent"
       }`}
     >
