4693202e36f8caf69116460062d8b35140f2b462
zingowalker <tsetangrace@gmail.com>
2024-02-15 17:08:33 +0530

    env

--- src/app/(about)/about/page.js
+++ src/app/(about)/about/page.js
@@ -5,36 +5,9 @@ export const metadata = {
   title: "About Us",
 };
 
-const jsonLd = {
-  "@context": "https://schema.org",
-  "@type": "Organization",
-  url: "https://www.ecoindiajourneys.com",
-  sameAs: ["https://www.instagram.com/ecoindiajourneys/"],
-  logo: "https://www.ecoindiajourneys.com/logoonsearch.png",
-  name: "Eco India Journeys",
-  description:
-    "We are committed to responsible & eco-tourism. Over 15 years of experience in Indian Himalayas.",
-  address: {
-    "@type": "PostalAddress",
-    streetAddress: "Eco India Journeys, near Jammu & Kashmir Bank, Leh, Ladakh",
-    addressLocality: "Leh",
-    addressCountry: "IN",
-    postalCode: "194101",
-  },
-  contactPoint: {
-    "@type": "ContactPoint",
-    email: "ecoindiajourneys@gmail.com",
-    telephone: "+91-880-077-7421",
-  },
-};
-
 export default function About() {
   return (
     <>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      />
       <AboutCoverSection />
       <Activities />
     </>
