c08db311a7985894268cb52af35c1cdb97f7fd2a
zingowalker <tsetangrace@gmail.com>
2024-05-16 17:30:39 +0530

    faq updated

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -8,68 +8,75 @@ const Faq = () => {
     {
       key: 1,
       title: "How does Eco India Journeys work?",
-      data: `Select your destination and then tell us what kind of experience you are looking for. We will connect you with a knowledgeable local travel specialist who will put together a tailor-made trip plan based on your requirements. Once you're 100% happy with the plan, you can book your trip via our secure payment system`,
+      data: `Select your destination and tell us what kind of experience you're looking for. Our knowledgeable local travel specialist will create a tailor-made trip plan based on your requirements. Once you're satisfied with the plan, you can book your trip securely through our payment system.`,
       isOpen: false,
     },
     {
       key: 2,
       title: "How much does it cost to use Eco India Journeys?",
-      data: `Eco India Journeys connects you directly with skilled local travel experts. No charges are applied until you confirm your trip with us. To ensure a secure payment gateway and top-tier customer service, a service fee is included in the overall trip cost. A fraction of this fee is allocated towards our commitment to environmental initiatives.`,
+      data: `The cost of your trip depends on various factors such as destination, duration, activities, and accommodations. Our travel specialists will provide you with a detailed quote based on your preferences.`,
       isOpen: false,
     },
     {
       key: 3,
       title: "What if I find cheaper prices elsewhere on the web?",
-      data: `At EIJ, our local experts curate deeply personalized journeys crafted specifically for your individual preferences and travel preferences. While striving to maintain competitive rates, we prioritize crafting top-tier trips by meticulously selecting accommodations, activities, and transportation. Our commitment is to deliver exceptional value, encompassing the expertise of your local specialist and seamless on-the-ground support throughout your travel, ensuring a hassle-free and enriching experience.`,
+      data: `We strive to offer competitive prices for our services. If you find a lower price for a similar package elsewhere, please let us know, and we'll do our best to match or beat it.`,
       isOpen: false,
     },
     {
       key: 4,
       title: "Are you like a traditional travel agent?",
-      data: `While cherishing the personal touch integral to outstanding travel agents in the planning and booking process, we transcend the typical 'travel agent' role. Our goal is to merge the finest attributes from both spheres: encompassing the personalized, hands-on service and profound destination expertise emblematic of traditional travel agents, along with cutting-edge messaging tech and advanced trip planning tools.`,
+      data: `While we offer similar services to traditional travel agents, we specialize in eco-friendly and sustainable travel experiences. Our focus is on creating unique and responsible journeys that benefit local communities and the environment.`,
       isOpen: false,
     },
     {
       key: 5,
-      title: "Does EIJ help book international flights?",
-      data: `At present, our primary focus revolves around enhancing the in-destination experience encompassing accommodations, transportation, and activities. Travelers are kindly requested to arrange their own international flight bookings. Nonetheless, our dedicated specialists remain available to address inquiries and offer guidance regarding preferred airports, optimal flight timings, and other related queries.`,
+      title: "Does Eco India Journeys help book international flights?",
+      data: `No, but yes we can assist you with booking international flights. Our travel specialists will help you find the best options that suit your preferences and budget.
+      `,
       isOpen: false,
     },
     {
       key: 6,
       title:
         "In the event that I am unable to travel, will I lose what I paid for my trip?",
-      data: `Typically, you have the flexibility to reschedule your trip without incurring extra charges provided you notify us at least two weeks prior to your scheduled departure. There's no obligation to confirm a new travel timeline until you're prepared to embark on your journey again. For further details, please feel free to contact our local specialists`,
+      data: `Our cancellation policy varies depending on the type of trip and the terms of our partners. We highly recommend purchasing travel insurance to protect your investment in case of unforeseen circumstances.
+      `,
       isOpen: false,
     },
     {
       key: 7,
       title: "How does payment work? Do I pay for my entire trip at once?",
-      data: `To secure your trip booking, a non-refundable deposit of 30% is necessary upon booking confirmation. The remaining balance is generally due one month before your trip commences, after which the trip becomes fully non-refundable. Should you desire an alternative payment arrangement, feel free to converse with us. For trips scheduled within one month from booking, full non-refundable payment is required at the time of booking to confirm all reservations.`,
+      data: `Payment terms may vary depending on the type of trip and the time of booking. Usually we ask for 50 % of the payment during tour confirmation and 50 % on arrival. Our travel specialists will provide you with detailed information on payment options and schedules.
+      `,
       isOpen: false,
     },
     {
       key: 8,
       title:
-        "In case of unforeseen last-minute alterations, what provisions does EIJ offer? Is travel insurance provided?",
-      data: `Our services do not incorporate travel insurance nonetheless, we strongly advise acquiring third-party travel insurance to safeguard against unforeseen cancellations or alterations beyond your control. While we strive to retrieve payments made to providers or rearrange affected services during unexpected circumstances, travel insurance offers additional coverage in scenarios where recovery is not feasible.
-        Should you face travel impossibility, in most instances, you can reschedule your trip at no extra cost by providing a minimum of two weeks' notice before your scheduled departure. You are not required to set new dates until you're prepared to resume your travel plans.
-        Typically, deposits, EIJ service fees, and planning charges are non-refundable. If cancellation becomes necessary, we will endeavor to recoup any reclaimable expenses. However, cancellation fees apply according to the date of written notification:
-        Over 1 month before departure → Loss of deposit, service fee, and planning fees paid
-        Less than 1 month before departure → No refund`,
+        "In case of unforeseen last-minute alterations, what provisions does Eco India Journeys offer? Is travel insurance provided?",
+      data: `We understand that travel plans can change unexpectedly. Depending on the circumstances, we will work with you to make necessary alterations to your trip. We recommend purchasing travel insurance to cover any unforeseen changes or cancellations.`,
       isOpen: false,
     },
     {
       key: 9,
       title:
-        "I've finalized bookings for my accommodations. Can I utilize EIJ for the remaining aspects of my trip?",
-      data: `EIJ specialists specialize in crafting comprehensive itineraries, handling transportation, accommodations, activities, and tours for travelers. Typically, they are unable to assist in forming partial itineraries if travelers have independently arranged specific elements. Coordinating multiple individuals managing different aspects of a trip can lead to a complex planning process!`,
+        "I've finalized bookings for my accommodations. Can I utilize Eco India Journeys for the remaining aspects of my trip?",
+      data: `Yes, you can still use Eco India Journeys to arrange other aspects of your trip such as transportation, activities, and guides. Our travel specialists will tailor the remaining components to complement your existing bookings.
+      `,
       isOpen: false,
     },
     {
       key: 10,
       title: "Which currencies and payment methods are accepted?",
-      data: `Although specializing in arranging trips for travelers worldwide, our company, based in the India, necessitates payments in INR. We will facilitate payment through a seamlessly integrated system soon, accepting major credit cards such as American Express, Visa, MasterCard, and Discover`,
+      data: `We accept various currencies and payment methods, including bank transfers and online payment platforms. Our travel specialists will provide you with specific details based on your location and preferences.`,
+      isOpen: false,
+    },
+    {
+      key: 11,
+      title: "Best Time to Visit Ladakh?",
+      data: `The best time to visit Ladakh is from May to September when the weather is favourable, and most of the tourist attractions are accessible. Avoid visiting during the winter months due to extreme cold and heavy snowfall, which may limit travel options.
+      `,
       isOpen: false,
     },
   ]);
